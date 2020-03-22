da505ff53e9739748107f0f6cddfaa938d9da560
Kelvin Omereshone <kelvinomereshone@gmail.com>
2020-03-22 09:56:13 +0100

    initial commit

---
+++ generator/template/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <ThemeProvider>
+    <CSSReset />
+    <!-- Uncomment below line if you are using vue-router -->
+
+    <!-- <router-view /> -->
+
+    <!--
+        Or just get started adding Chakra UI components
+    -->
+  </ThemeProvider>
+</template>
+
+<script>
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+
+export default {
+  name: "App",
+  components: {
+    ThemeProvider,
+    CSSReset
+  }
+};
+</script> 
