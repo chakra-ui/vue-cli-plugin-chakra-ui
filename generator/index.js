1f8f997900a5ae55b2818c7d0ca3191baa4f3ffa
Kelvin Omereshone <kelvinomereshone@gmail.com>
2020-05-07 14:24:15 +0100

    fix: make npm always download latest version of chakra ui and peer dependency

--- generator/index.js
+++ generator/index.js
@@ -1,37 +1,34 @@
 module.exports = (api, options) => {
-    api.extendPackage({
-        dependencies: {
-            "@chakra-ui/vue": "^0.3.1",
-            emotion: "^10.0.27"
-        }
-    });
+  api.extendPackage({
+    dependencies: {
+      "@chakra-ui/vue": "*",
+      emotion: "*",
+    },
+  });
 
-    api.injectImports(
-        api.entryFile,
-        `import Chakra from '@chakra-ui/vue'`
-    );
+  api.injectImports(api.entryFile, `import Chakra from '@chakra-ui/vue'`);
 
-    api.render('./template', {
-        hasVueRouter: options.hasVueRouter
-    })
-    api.onCreateComplete(() => {
-        let vueUseLine = `\n\nVue.use(Chakra)`
+  api.render("./template", {
+    hasVueRouter: options.hasVueRouter,
+  });
+  api.onCreateComplete(() => {
+    let vueUseLine = `\n\nVue.use(Chakra)`;
 
-        const fs = require('fs')
+    const fs = require("fs");
 
-        let contentMain = fs.readFileSync(api.entryFile, { encoding: 'utf-8' })
+    let contentMain = fs.readFileSync(api.entryFile, { encoding: "utf-8" });
 
-        const lines = contentMain.split(/\r?\n/g).reverse();
+    const lines = contentMain.split(/\r?\n/g).reverse();
 
-        const lastImportIndex = lines.findIndex(line => line.match(/^import/));
+    const lastImportIndex = lines.findIndex((line) => line.match(/^import/));
 
-        lines[lastImportIndex] += vueUseLine;
+    lines[lastImportIndex] += vueUseLine;
 
-        // modify app
-        contentMain = lines.reverse().join('\n');
+    // modify app
+    contentMain = lines.reverse().join("\n");
 
-        fs.writeFileSync(api.entryFile, contentMain, { encoding: 'utf-8' });
-    })
+    fs.writeFileSync(api.entryFile, contentMain, { encoding: "utf-8" });
+  });
 
-    api.exitLog('Chakra UI Vue is ready')
-}
+  api.exitLog("Chakra UI Vue is ready");
+};
